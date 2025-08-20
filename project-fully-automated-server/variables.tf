b7e9424f854d3706bcb25320325cfa22b88c7fc2
toandaocloud <tdaok6@gmail.com>
2025-08-20 18:51:15 +0700

    feat: Add Project 2 - Fully Automated Web Server with Terraform

---
+++ project-fully-automated-server/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+description = " the aws region to deploy resources in"
+type = string 
+}
+variable "domain_name" {
+description = " the domain name you own and want to use"
+type = string
+}
